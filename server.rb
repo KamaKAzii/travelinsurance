f74b158938d076c615a584df2c39da5d45dc7415
Emmet William Rogan <spskamakazii@gmail.com>
2013-10-03 18:53:19 +1000

    Add some mobile specific tags.

---
+++ server.rb
@@ -0,0 +1,21 @@
+gem 'compass'
+
+require 'compass'
+require 'sinatra'
+require 'sass'
+
+set :bind, '0.0.0.0'
+
+configure do
+  Compass.configuration do |config|
+    config.project_path = File.dirname(__FILE__)
+    config.sass_dir = 'views'
+  end
+
+  set :sass, Compass.sass_engine_options
+end
+
+get "/stylesheets/screen.css" do
+  content_type 'text/css'
+  sass :screen
+end
