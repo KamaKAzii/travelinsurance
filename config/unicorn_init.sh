148be8f7f34911743c444157d1cfbf61d7b223e3
Emmet William Rogan <spskamakazii@gmail.com>
2013-10-09 20:52:33 +1100

    Try namespace the pid.

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -6,7 +6,7 @@ set -e
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/deployer/apps/travelinsurance/current
-PID=$APP_ROOT/tmp/pids/unicorn.pid
+PID=$APP_ROOT/tmp/pids/unicorn.travelinsurance.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c config/unicorn.rb -E production"
 AS_USER=deployer
 action="$1"
