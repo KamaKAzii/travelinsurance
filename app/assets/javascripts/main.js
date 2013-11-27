506a3777927e765f6730a950ba3d0249976875f6
Emmet William Rogan <spskamakazii@gmail.com>
2013-11-27 22:22:15 +1100

    Clean up application layout javascript calls and also the main.js file.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,22 +1,3 @@
 $(function() {
   FastClick.attach(document.body);
 });
-
-ti.utils = {
-  hideBrowserAddressBar: function() {
-    setTimeout(function(){
-      window.scrollTo(0, 0);
-      }, 0);
-  }
-};
-
-$(document).ready(function() {
-  // Deals with setting up page height and breadcrumb.
-  ti.controller.initPage();
-
-  // First controller action after breadcrumb is finished
-  $("header").on("breadcrumbLoadAnimationFinished", function() {
-    ti.controller.open();
-  });
-
-});
