582f10ec384c38227c491ac70d6bc02794dffe21
Emmet William Rogan <spskamakazii@gmail.com>
2013-12-01 16:22:20 +1100

    Enhance quote model initial spec.

--- spec/models/quote_spec.rb
+++ spec/models/quote_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe "Quote" do
   it "Can be created with a date of today" do
     quote = Quote.create
-    
+    expect(quote).to be_valid
     expect(quote.default_date).to eq(Date.now)
   end
 end
